de4a6b732ccf696774636a246ca3922fc623b0e8
aoitchan0212 <aoi.t.chan0212@gmail.com>
2025-05-26 10:07:18 +0900

    確認問題３.３

--- src/RectTest.java
+++ src/RectTest.java
@@ -3,9 +3,24 @@ public class RectTest extends MyFrame {
 
 	public void run() {
 		// TODO 自動生成されたメソッド・スタブ
-		fillRect(30,50,200,100);
+		setColor(148,0,211);
+		fillRect(50,50,50,50);
 		
-
+		setColor(255,255,0);
+		fillRect(100,80,50,50);
+		
+		setColor(135,206,235);
+		fillRect(50,130,50,50);
+		
+		setColor(105,105,105);
+		fillRect(100,150,50,50);
+		
+		setColor(0,0,0);
+		fillRect(50,200,50,50);
+		
+		setColor(255,255,255);
+		fillRect(100,250,50,50);
+		
 	}
 
 }
