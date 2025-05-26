e72e220d6ac550f2042499c96f60d7e88a705fa4
aoitchan0212 <aoi.t.chan0212@gmail.com>
2025-05-26 10:50:04 +0900

    OvalTestの追加

--- src/Start.java
+++ src/Start.java
@@ -4,7 +4,7 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("コメント");
-		MyFrame frame1=new RectTest();
+		MyFrame frame1=new OvalTest();
 
 	}
 
