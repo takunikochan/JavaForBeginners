562afcf8ec7429b88922ca309124f730170e441f
aoitchan0212 <aoi.t.chan0212@gmail.com>
2025-05-27 10:40:19 +0900

    DisplayRectsSlowの追加、確認問題5.1

---
+++ src/DisplayRectsSlow.java
@@ -0,0 +1,16 @@
+
+public class DisplayRectsSlow extends MyFrame{
+	public void run() {
+		setColor(0,128,0);
+		int x=30;
+		int y=80;
+		while (x<=170) {
+			fillRect(x,y,10,100);
+			x+=15;
+			y+=15;
+			sleep(1);
+		}
+		
+	}
+
+}
