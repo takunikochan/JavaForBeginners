f5844889ba2b5bacd381ad7468a11626f6aa39eb
aoitchan0212 <aoi.t.chan0212@gmail.com>
2025-05-27 12:26:33 +0900

    練習問題5-1,5-2,5-3

--- src/Animation.java
+++ src/Animation.java
@@ -1,16 +1,77 @@
 
 public class Animation extends MyFrame{
 	public void run() {
+		//【確認問題5-1】
+	//	int x=30;
+	//	int y=180;
+	//	
+	//	while(y>=50) {
+	//		clear();
+	//		setColor(0,128,0);
+	//		fillRect(x,y,10,100);
+	//		y-=5;
+	//		sleep(0.1);
+	//	}
+	//	while(x<=170) { 
+	//		// 画面を消す
+	//		clear();
+	//		// 四角形を表示する
+	//		setColor(0,128,0);
+	//		fillRect(x,y,10,100);
+	//		x+=5;
+	//		sleep(0.1);
+	//	}
 		
-		int x=30;
-		while(x<=170) {
-			// 画面を消す
-			clear();
-			// 四角形を表示する
+		//【確認問題5-2】
+		
+	/*	int x=30;
+		int y=50;
+		
+		while(y<=250){
+	     clear();
+		setColor(0,128,0);
+		fillRect(x,y,10,100);
+		x+=5;
+		y+=5;
+		sleep(0.1);
+		}
+		while(x>=30){
+		     clear();
 			setColor(0,128,0);
-			fillOval(x,80,30,30);
-			x+=30;
+			fillRect(x,y,10,100);
+			x-=5;
 			sleep(0.1);
+	     }
+		while(x<=250){
+		     clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			x+=5;
+			y-=5;
+			sleep(0.1);
+			}
+	*/
+		
+		//【確認問題5-3】
+		int y=100;
+		int x=30;
+		
+		for(int i=0 ; i<4 ; i++) {
+		while(x<=170) {
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			x+=5;
+			sleep(0.1);
+		}
+			while(x>=30) {
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			x-=5;
+			sleep(0.1);
+			
+		}
 		}
 	}
 }
