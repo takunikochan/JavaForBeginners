d2e458b228a22b38dc6bf253cfdcf9cb76f8e2e0
aoitchan0212 <aoi.t.chan0212@gmail.com>
2025-05-27 09:41:05 +0900

    練習問題4-2

--- src/RectIf.java
+++ src/RectIf.java
@@ -2,15 +2,15 @@
 public class RectIf extends MyFrame{
 	public void run() {
 		
-		int x=30;
-		int y=30;
+		int x=50;
+		int y=50;
 		setColor(0,128,0);
 		int i;
 		for( i=0 ; i<10 ; i++) {
-		   fillRect(x,y,10,100);
-			x+=15;
+		   fillRect(y,30,10,x);
+			x+=20;
 			y+=20;
-			fillRect(x,y,10,100);
+			fillRect(y,30,10,x);
 		
 		}
 	
