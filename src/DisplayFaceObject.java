73a25ff8970492a2053dfc029b55f7174b6907cc
aoitchan0212 <aoi.t.chan0212@gmail.com>
2025-06-02 11:41:07 +0900

    6.3 追加

--- src/DisplayFaceObject.java
+++ src/DisplayFaceObject.java
@@ -1,17 +1,19 @@
 
 public class DisplayFaceObject extends MyFrame{
 	public void run() {
-		Face facel=new Face(50,50);
+		Face face1=new Face(50,50);
+		Face face2=new Face(200,150);
 		
-		facel.draw(this);
-		
-		Face facel2=new Face(150,150);
-		
-		facel2.draw(this);
-		
-         Face facel3=new Face(250,50);
-		
-		facel3.draw(this);
+      for(int i=0; i<30; i++) {
+    	  clear();
+    	  face1.draw(this);
+    	  face2.draw(this);
+    	  face1.x+=10;
+    	  face1.y+=10;
+    	  face2.x-=10;
+    	  face2.y-=10;
+    	  sleep(0.05);
+      }
 		
 	}
 	
